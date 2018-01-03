0e2035ce5e4ee8bc8f0dbba958ae35e6d3d3d42c
Paula Muldoon <paula.muldoon@kurtgeiger.com>
2018-01-03 10:28:17 +0000

    add backstage pass code to pass tests

--- spec/backstage_pass_spec.js
+++ spec/backstage_pass_spec.js
@@ -1,73 +1,66 @@
-// describe("Backstage Pass", function() {
-//    beforeEach(function() {
-//        pass = new BackstagePass("Backstage passes", 11, 3);
-//        pass2 = new BackstagePass("Backstage passes", 10, 3);
-//        pass3 = new BackstagePass("Backstage passes", 5, 3);
-//        pass4 = new BackstagePass("Backstage passes", 0, 10);
-//        pass5 = new BackstagePass("Backstage passes", 2, 50);
-//        gildedRose = new Inn([pass, pass2, pass3, pass4, pass5]);
-//    });
-//
-//     describe("product creation", function() {
-//         it("can be created with a name", function() {
-//             expect(pass.name).toEqual("Backstage passes");
-//         });
-//
-//         it("can be created with a sell-in", function() {
-//             expect(pass.sellIn).toEqual(11);
-//         });
-//
-//         it("can be created with a quality", function() {
-//             expect(pass.quality).toEqual(3);
-//         });
-//
-//         xit("can be created with a max quality of 50", function() {
-//             expect(new BackstagePass("Pass", 5, 51)).toThrow(new Error("Max quality is 50."));
-//         });
-//
-//         xit("can be created with a minimum quality of 0", function() {
-//             expect(new BackstagePass("Pass", 5, -1)).toThrow(new Error("Minimum quality is 0."));
-//         });
-//     });
-//
-//     describe("sell-in", function() {
-//         it("decreases sell-in by 1", function() {
-//             gildedRose.updateItems();
-//             expect(gildedRose.items[0].sellIn).toEqual(10);
-//         })
-//     });
-//
-//     describe("quality", function() {
-//
-//         beforeEach(function() {
-//             gildedRose.updateItems();
-//         });
-//
-//         it("increases in quality by 1 with >10 days before concert", function() {
-//             expect(gildedRose.items[0].quality).toEqual(4);
-//         });
-//
-//         it("increases in quality by 2 with <= 10 days before concert", function() {
-//             expect(gildedRose.items[1].quality).toEqual(5);
-//         });
-//
-//         it("increases in quality by 3 with <= 5 days before concert", function() {
-//            expect(gildedRose.items[2].quality).toEqual(6);
-//         });
-//
-//         it("has 0 quality after concert", function() {
-//            expect(gildedRose.items[3].quality).toEqual(0);
-//         });
-//
-//         it("has a max quality of 50", function() {
-//            expect(gildedRose.items[4].quality).toEqual(50);
-//         });
-//
-//         xit("cannot have a negative quality", function() {
-//             for (let i = 0; i < 2; i++) {
-//                 gildedRose.updateItems()§;
-//             }
-//             expect(gildedRose.items[1].quality).toEqual(0);
-//         });
-//     });
-// });
+describe("Backstage Pass", function() {
+   beforeEach(function() {
+       pass = new BackstagePass("Backstage passes", 11, 3);
+       pass2 = new BackstagePass("Backstage passes", 10, 3);
+       pass3 = new BackstagePass("Backstage passes", 5, 3);
+       pass4 = new BackstagePass("Backstage passes", 0, 10);
+       pass5 = new BackstagePass("Backstage passes", 2, 50);
+       gildedRose = new Inn([pass, pass2, pass3, pass4, pass5]);
+   });
+
+    describe("product creation", function() {
+        it("can be created with a name", function() {
+            expect(pass.name).toEqual("Backstage passes");
+        });
+
+        it("can be created with a sell-in", function() {
+            expect(pass.sellIn).toEqual(11);
+        });
+
+        it("can be created with a quality", function() {
+            expect(pass.quality).toEqual(3);
+        });
+
+        xit("can be created with a max quality of 50", function() {
+            expect(new BackstagePass("Pass", 5, 51)).toThrow(new Error("Max quality is 50."));
+        });
+
+        xit("can be created with a minimum quality of 0", function() {
+            expect(new BackstagePass("Pass", 5, -1)).toThrow(new Error("Minimum quality is 0."));
+        });
+    });
+
+    describe("sell-in", function() {
+        it("decreases sell-in by 1", function() {
+            gildedRose.updateItems();
+            expect(gildedRose.items[0].sellIn).toEqual(10);
+        })
+    });
+
+    describe("quality", function() {
+
+        beforeEach(function() {
+            gildedRose.updateItems();
+        });
+
+        it("increases in quality by 1 with >10 days before concert", function() {
+            expect(gildedRose.items[0].quality).toEqual(4);
+        });
+
+        it("increases in quality by 2 with <= 10 days before concert", function() {
+            expect(gildedRose.items[1].quality).toEqual(5);
+        });
+
+        it("increases in quality by 3 with <= 5 days before concert", function() {
+           expect(gildedRose.items[2].quality).toEqual(6);
+        });
+
+        it("has 0 quality after concert", function() {
+           expect(gildedRose.items[3].quality).toEqual(0);
+        });
+
+        it("has a max quality of 50", function() {
+           expect(gildedRose.items[4].quality).toEqual(50);
+        });
+    });
+});
--- src/backstage_pass.js
+++ src/backstage_pass.js
@@ -6,6 +6,27 @@ class BackstagePass {
     }
 
     updateItems() {
+        this.updateQuality();
+        this.lowerSellIn();
+    }
 
+    lowerSellIn() {
+        this.sellIn -= 1;
+    }
+
+    updateQuality() {
+        if (this.sellIn > 10) {
+            this.quality += 1;
+        } else if (this.sellIn <= 0) {
+            this.quality = 0;
+        } else if (this.sellIn <= 5) {
+            this.quality += 3;
+        } else if (this.sellIn <= 10) {
+            this.quality += 2;
+        }
+
+        if (this.quality > 50) {
+            this.quality = 50;
+        }
     }
 }
