3861e0cecabb0e431582bf5e8d46474634023f2e
Paula Muldoon <paula.muldoon@kurtgeiger.com>
2018-01-04 11:03:16 +0000

    refactor aged brie names to match array index

--- spec/aged_brie_spec.js
+++ spec/aged_brie_spec.js
@@ -3,22 +3,22 @@ describe("Aged Brie", function() {
     const anonymousSellIn = 5;
 
     beforeEach(function() {
-        brie1 = new AgedBrie("Aged Brie", 4, 2);
-        brie2 = new AgedBrie("Aged Brie", 4, 50);
-        gildedRose = new Inn([brie1, brie2]);
+        brie0 = new AgedBrie("Aged Brie", 4, 2);
+        brie1 = new AgedBrie("Aged Brie", 4, 50);
+        gildedRose = new Inn([brie0, brie1]);
     });
 
     describe("Aged Brie creation", function() {
        it("can be created with a name", function() {
-          expect(brie1.name).toEqual("Aged Brie");
+          expect(brie0.name).toEqual("Aged Brie");
        });
 
        it("can be created with sell-in", function() {
-          expect(brie1.sellIn).toEqual(4);
+          expect(brie0.sellIn).toEqual(4);
        });
 
        it("can be created with quality", function() {
-           expect(brie1.quality).toEqual(2);
+           expect(brie0.quality).toEqual(2);
        });
 
        it("can be created with a max quality of 50", function() {
@@ -37,19 +37,19 @@ describe("Aged Brie", function() {
     describe("sell-in", function() {
         it("decreases the sell-in by 1", function() {
             gildedRose.updateItems();
-            expect(brie1.sellIn).toEqual(3);
+            expect(brie0.sellIn).toEqual(3);
         });
     });
 
     describe("quality", function() {
        it("increases in quality with age", function() {
            gildedRose.updateItems();
-           expect(brie1.quality).toEqual(3);
+           expect(brie0.quality).toEqual(3);
        });
 
        it("has a max quality of 50", function() {
            gildedRose.updateItems();
-           expect(brie2.quality).toEqual(50);
+           expect(brie1.quality).toEqual(50);
        })
     });
 });
