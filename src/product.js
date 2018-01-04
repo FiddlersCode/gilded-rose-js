f8e317c4ad2048e61b617ee67955223b45e5d8ba
Paula Muldoon <paula.muldoon@kurtgeiger.com>
2018-01-04 15:27:49 +0000

    Refactor product.js  - remove redundant if statement  - add Math.max function

--- src/product.js
+++ src/product.js
@@ -22,15 +22,10 @@ class Product {
     reduceQuality() {
         if (this.sellIn < 0) {
             this.quality -= 2;
-        } else if (this.quality < 0) {
-            this.quality = 0;
-        }
-        else {
+        } else {
             this.quality -= 1;
         }
 
-        if (this.quality < 0) {
-            this.quality = 0;
-        }
+        this.quality = Math.max(0, this.quality);
     }
 }
